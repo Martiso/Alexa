fe7acc15d174046fa2df6f682899d503cfeb71dd
Janek <martiso060@gmail.com>
2018-12-22 17:38:55 +0100

    alexasyntezator

--- static/skills.js
+++ static/skills.js
@@ -6,14 +6,30 @@ const skille = [
         }
     },
     {
-        prefix: 'wasza komenda',
+        prefix: 'mów',
         skill: function(){
-            //wasz prorgam
-        }
+            msg.text = `Litwo, Ojczyzno moja!`
+             window.speechSynthesis.speak(msg)
+       }
+    },
+    {
+       prefix: 'bitboksuje',
+       skill: function(){
+           msg.text = `pumpumcympumpumpumcympuiłuiłuiłuiłupumpumcympumpumpumcympuiłuiłuiłuiłupumpumcympumpumpumcympuiłuiłuiłuiłupumpumcympumpumpumcympuiłuiłuiłuiłupumpumcympumpumpumcympuiłuiłuiłuiłupumpumcympumpumpumcympuiłuiłuiłuiłupumpumcympumpumpumcympuiłuiłuiłuiłupumpumcympumpumpumcympuiłuiłuiłuiłupumpumcympumpumpumcympuiłuiłuiłuiłupumpumcympumpumpumcympuiłuiłuiłuiłupumpumcympumpumpumcympuiłuiłuiłuiłupumpumcympumpumpumcympuiłuiłuiłuiłupumpumcympumpumpumcympuiłuiłuiłuiłupumpumcympumpumpumcympuiłuiłuiłuiłupumpumcympumpumpumcympuiłuiłuiłuiłupumpumcympumpumpumcympuiłuiłuiłuiłupumpumcympumpumpumcympuiłuiłuiłuiłupumpumcympumpumpumcympuiłuiłuiłuiłupumpumcympumpumpumcympuiłuiłuiłuiłupumpumcympumpumpumcympuiłuiłuiłuiłupumpumcympumpumpumcympuiłuiłuiłuiłu`
+           window.speechSynthesis.speak(msg)
+        }      
     }
 ];
 
+let voices = []
+const msg = new SpeechSynthesisUtterance();
 function execute(command){
     const run = skille.find( (skill)=> command.includes(skill.prefix) );
     run.skill()
 }
+
+
+window.speechSynthesis.addEventListener('voicechanged', (e)=>{
+    voices = window.speechSynthesis.getVoices();
+    msg.lang = voices [0];
+})
